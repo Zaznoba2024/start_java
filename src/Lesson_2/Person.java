404f26783a3510999727cecd6411a962da132837
kushnina <zaznoba.ek@yandex.ru>
2024-08-06 11:48:35 +0500

    Lesson02

--- src/Lesson_2/Person.java
+++ src/Lesson_2/Person.java
@@ -10,15 +10,19 @@ public class Person {
     void walk() {
         System.out.println("идёт");
     }
+
     void sit() {
         System.out.println("сидит");
     }
+
     void run() {
         System.out.println("бежит");
     }
+
     void say() {
         System.out.println("говорит");
     }
+
     void studyJava() {
         System.out.println("учит Java");
     }
