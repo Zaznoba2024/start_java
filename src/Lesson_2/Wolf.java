e6739f607551f8728dca4566ccf9f5ff68b4356f
kushnina <zaznoba.ek@yandex.ru>
2024-08-06 19:45:20 +0500

    Lesson02_wolf

---
+++ src/Lesson_2/Wolf.java
@@ -0,0 +1,29 @@
+package Lesson_2;
+
+public class Wolf {
+    String gender;
+    String name;
+    int weight;
+    int age;
+    String color;
+
+    void walk() {
+        System.out.println("Идёт");
+    }
+
+    void sit() {
+        System.out.println("Сидит");
+    }
+
+    void run() {
+        System.out.println("Бежит");
+    }
+
+    void howl() {
+        System.out.println("Воет");
+    }
+
+    void hunt() {
+        System.out.println("Охотится");
+    }
+}
