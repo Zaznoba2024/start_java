3f03fa9744006accbec0f586eb59b405cc4c5a3a
kushnina <zaznoba.ek@yandex.ru>
2024-08-06 11:47:17 +0500

    Lesson02

---
+++ src/Lesson_2/Person.java
@@ -0,0 +1,25 @@
+package Lesson_2;
+
+public class Person {
+    String gender = "female";
+    String name = "Софья";
+    int height = 172;
+    int weight = 58;
+    int age = 28;
+
+    void walk() {
+        System.out.println("идёт");
+    }
+    void sit() {
+        System.out.println("сидит");
+    }
+    void run() {
+        System.out.println("бежит");
+    }
+    void say() {
+        System.out.println("говорит");
+    }
+    void studyJava() {
+        System.out.println("учит Java");
+    }
+}
