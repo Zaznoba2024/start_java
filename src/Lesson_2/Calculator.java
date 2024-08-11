9385b8caa10682dbf69f3e26608c1676d1190cfb
kushnina <zaznoba.ek@yandex.ru>
2024-08-11 12:30:10 +0500

    Lesson02_calculator

--- src/Lesson_2/Calculator.java
+++ src/Lesson_2/Calculator.java
@@ -4,71 +4,66 @@ public class Calculator {
     private int num1;
     private int num2;
     private char mathOperation;
-
-    public int getNum1() {
-        return num1;
-    }
+    double result;
+    public String inputResult;
 
     public void setNum1(int num1) {
         this.num1 = num1;
-    }
-
-    public int getNum2() {
-        return num2;
     }
 
     public void setNum2(int num2) {
         this.num2 = num2;
     }
 
-    public char getMathOperation() {
-        return mathOperation;
-    }
-
     public void setMathOperation(char mathOperation) {
-        this.mathOperation = mathOperation;
+        if (mathOperation != '+' && mathOperation != '-' &&
+                mathOperation != '*' && mathOperation != '/' &&
+                mathOperation != '^' && mathOperation != '%') {
+            inputResult = "Ошибка ввода";
+        } else {
+            this.mathOperation = mathOperation;
+            inputResult = "";
+        }
     }
 
     public double calculate() {
-        double result = num1;
         switch (mathOperation) {
-            case ('+'):
-                result += num2;
-                break;
-            case ('-'):
-                result -= num2;
-                break;
-            case ('*'):
-                result *= num2;
-                break;
-            case ('/'):
+            case '+':
+                return result = num1 + num2;
+            case '-':
+                return result = num1 - num2;
+            case '*':
+                return result = num1 * num2;
+            case '/':
+            case '%':
                 if (num2 == 0) {
                     System.out.println("Ошибка: деление на ноль запрещено");
-                    return Double.NaN;
-                } else {
-                    result /= num2;
+                    return result = Double.NaN;
                 }
-                break;
-            case ('%'):
-                if (num2 == 0) {
-                    System.out.println("Ошибка: деление на ноль запрещено");
-                    return Double.NaN;
-                } else {
-                    result %= num2;
+                if (mathOperation == '/') {
+                    return result = num1 / num2;
                 }
-                break;
-            case ('^'):
+                return result = num1 % num2;
+            case '^':
                 if (num2 == 0) {
-                    result = 1;
-                } else {
-                    for (int i = 1; i < Math.abs(num2); i++) {
-                        result *= num1;
-                    }
+                    return result = 1;
+                }
+                result = num1;
+                for (int i = 1; i < Math.abs(num2); i++) {
+                    result *= num1;
                 }
                 if (num2 < 0) {
-                    result = 1 / result;
+                    return result = 1 / result;
                 }
+                return result;
+            default:
+                throw new IllegalStateException("Некорректная операция: " + mathOperation);
         }
-        return result;
+    }
+
+    public void print() {
+        if (!Double.isNaN(result)) {
+            System.out.printf("%s %c %s = %s\n", num1, mathOperation, num2, result);
+        }
     }
 }
--- src/Lesson_2/CalculatorTest.java
+++ src/Lesson_2/CalculatorTest.java
@@ -7,42 +7,32 @@ public class CalculatorTest {
         Calculator calculator = new Calculator();
         Scanner scanner = new Scanner(System.in);
 
-        boolean hasContinue = true;
-
+        String answer = "";
         do {
             System.out.print("Введите первое число: ");
             calculator.setNum1(scanner.nextInt());
-            System.out.print("Введите знак операции (+, -, *, /, ^, %): ");
-            char operation = scanner.next().charAt(0);
-            while ((operation != '+' && operation != '-' &&
-                    operation != '*' && operation != '/' &&
-                    operation != '^' && operation != '%')) {
-                System.out.print("Ошибка: операция '" + operation + "' не поддерживается.\n" +
-                        "Доступны следующие операции: +, -, *, /, ^, %\n" +
-                        "Введите знак операции (+, -, *, /, ^, %): ");
-                operation = scanner.next().charAt(0);
-            }
-            calculator.setMathOperation(operation);
+            do {
+                System.out.print("Введите знак операции (+, -, *, /, ^, %): ");
+                char operation = scanner.next().charAt(0);
+                calculator.setMathOperation(operation);
+                if ("Ошибка ввода".equals(calculator.inputResult)) {
+                    System.out.print("Ошибка: операция '" + operation + "' не поддерживается.\n" +
+                            "Доступны следующие операции: +, -, *, /, ^, %\n");
+                }
+            } while ("Ошибка ввода".equals(calculator.inputResult));
             System.out.print("Введите второе число: ");
             calculator.setNum2(scanner.nextInt());
 
-            double result = calculator.calculate();
-            if (!Double.isNaN(result)) {
-                System.out.printf("%s %c %s = %s\n", calculator.getNum1(),
-                        calculator.getMathOperation(), calculator.getNum2(), result);
-            }
+            calculator.calculate();
+            calculator.print();
 
-            while (true) {
+            while (!"no".equals(answer)) {
                 System.out.print("Хотите продолжить вычисления? [yes/no]: ");
-                String answer = scanner.next();
+                answer = scanner.next();
                 if ("yes".equals(answer)) {
                     break;
                 }
-                if ("no".equals(answer)) {
-                    hasContinue = false;
-                    break;
-                }
             }
-        } while (hasContinue);
+        } while (!"no".equals(answer));
     }
 }
